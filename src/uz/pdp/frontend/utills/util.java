5e5033351264f9a6986b0035b16e72749921f99e
Khurshid <muxsimovxurshid@gmail.com>
2024-04-18 18:34:37 +0500

    util for console ui

---
+++ src/uz/pdp/frontend/utills/util.java
@@ -0,0 +1,45 @@
+package uz.pdp.frontend.utills;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class util {
+    private static final Scanner scanInt = new Scanner(System.in);
+    private static final Scanner scanStr = new Scanner(System.in);
+
+    public static int getInt(String hint) {
+        int num;
+        while (true) {
+            try {
+                System.out.print(hint);
+                num = scanInt.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid Input! Please enter an integer.");
+                scanInt.nextLine();
+            }
+        }
+        return num;
+    }
+
+    public static String getStr(String hint){
+        System.out.print(hint);
+        return scanStr.nextLine();
+    }
+
+    public static double getDouble(String hint) {
+        double num;
+        while (true) {
+            try {
+                System.out.print(hint);
+                num = scanInt.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect input! Please enter a valid number.");
+                scanInt.nextLine();
+            }
+        }
+        return num;
+    }
+
+}
