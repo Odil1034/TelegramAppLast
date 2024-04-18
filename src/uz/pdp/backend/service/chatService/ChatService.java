6f3e541cf5d7cf5a6254a90f47ee8569b8914125
Khurshid <muxsimovxurshid@gmail.com>
2024-04-18 16:11:24 +0500

    create chat service

---
+++ src/uz/pdp/backend/service/chatService/ChatService.java
@@ -0,0 +1,8 @@
+package uz.pdp.backend.service.chatService;
+
+import uz.pdp.backend.models.chat.Chat;
+import uz.pdp.backend.service.BaseService;
+
+public interface ChatService extends BaseService<Chat> {
+
+}
---
+++ src/uz/pdp/backend/service/chatService/ChatServiceImp.java
@@ -0,0 +1,57 @@
+package uz.pdp.backend.service.chatService;
+
+import uz.pdp.backend.models.chat.Chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServiceImp implements ChatService {
+    private static ChatService chatService;
+    private final List<Chat> chatList;
+
+
+
+
+    private ChatServiceImp() {
+        chatList = new ArrayList<>();
+    }
+
+    public static ChatService getInstance() {
+        if (chatService == null) {
+            chatService = new ChatServiceImp();
+        }
+        return chatService;
+    }
+
+
+    @Override
+    public boolean create(Chat chat) {
+        chatList.add(chat);
+        return true;
+    }
+
+    @Override
+    public Chat get(String ID) {
+        for (Chat chat : chatList) {
+            if (chat.getID().equals(ID)) {
+                return chat;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Chat> getList() {
+        return chatList;
+    }
+
+    @Override
+    public void update(Chat newM) {
+        //???
+    }
+
+    @Override
+    public boolean delete(String ID) {
+        return chatList.removeIf(chat -> chat.getID().equals(ID));
+    }
+}
