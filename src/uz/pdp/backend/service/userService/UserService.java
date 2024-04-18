1477856705eb74774f7530c103ea961f1983b640
Khurshid <muxsimovxurshid@gmail.com>
2024-04-18 16:07:08 +0500

    add method getUserByUsername

--- src/uz/pdp/backend/service/userService/UserService.java
+++ src/uz/pdp/backend/service/userService/UserService.java
@@ -4,9 +4,13 @@ import uz.pdp.backend.DTO.LoginDTO;
 import uz.pdp.backend.models.user.User;
 import uz.pdp.backend.service.BaseService;
 
+import java.util.List;
+
 public interface UserService extends BaseService<User> {
 
     User login(LoginDTO loginDTO);
     User signUp(User user);
+    User getUserByUsername(String username);
+
 
 }
