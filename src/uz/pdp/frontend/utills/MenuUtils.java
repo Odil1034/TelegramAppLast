8cd0efedea6578c446eaa3f9ec59bccd16dd1edd
Khurshid <muxsimovxurshid@gmail.com>
2024-04-19 19:19:59 +0500

    user menuni ozgartrdm shu tori boladi manmcha

--- src/uz/pdp/frontend/utills/MenuUtils.java
+++ src/uz/pdp/frontend/utills/MenuUtils.java
@@ -3,19 +3,44 @@ package uz.pdp.frontend.utills;
 public interface MenuUtils {
 
     // main Menu
-    String MAIN = "MAIN MENU \n1. Login In \n2. Sign Up \n3. Exit";
-    String ADMIN_MENU = "1. Show users (soni) \n2. Block or unblock user\n3. Write message " +
-                       "\n4. Delete message \n5. Change group name \n6. Change group picture " +
-                       "\n7. Add admin \n8. Chat history \n9. Search user \n10. Delete and leave group";
+    String MAIN = """
+            ==========================================================
+            MAIN MENU\s
+            1. Login In\s
+            2. Sign Up\s
+            3. Exit
+            ==========================================================""";
+    String ADMIN_MENU = """
+            ==========================================================
+            1. Show users (%d)\s
+            2. Block or unblock user\s
+            3. Write message\s
+            4. Delete message\s
+            5. Change group name\s
+            6. Change group picture\s
+            7. Add admin\s
+            8. Chat history\s
+            9. Search user\s
+            10. Delete and leave group
+            ==========================================================""";
 
-    String USER_MENU = "\n1. Show message \n2. Write message \n3. Edit message " +
-                       "\n4. Delete message \n5. Add user(Lichniy users) \n6. Save user's contact" +
-                       "\n7. Search message \n8. Search chat \n9. Search user " +
-                       "\n10. Block user";
+    String USER_MENU = """
+            ==========================================================
+            1. All\s
+            2. Chats\s
+            3. Groups\s
+            4. Channels\s
+            5. Create group\s
+            6. New Chat\s
+            7. Create channel\s
+            8. My profile\s
+            9. Settings\s
+            10. Log Out\s
+            ==========================================================""";
 
 
     static int menu(String menu){
         System.out.println(menu);
-        return InputStream.getInt("Choose: ");
+        return InputStream.getInt("choice: ");
     }
 }
